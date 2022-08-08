c4aea413cf802187ebe0dc6cfd8a34c1a48bf14e
madgicaltarun <tarun.rai@madgicaltechdom.com>
2022-08-08 14:45:47 +0530

    completed lecture 16 of module first

---
+++ Javascript fundamental part -1/script.js
@@ -0,0 +1,100 @@
+/*
+let js = "amazing";
+console.log("Tarun Rai"); //Tarun is value
+
+let firstName = "tarun";
+
+let tarun_rai = "TR";
+
+let $function = 27;
+
+//variable name conventions
+
+let person = "Tarun";
+
+let PI = 3.1415; // make a constant varible in upper case for good practice
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Tarun");
+
+javascriptIsFun = "YEs!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+
+let age = 30;
+age = 31;
+
+const birthYear = 1997;
+birthYear = 1222;
+
+
+// Math Operators
+const now = 2037;
+const ageTarun = now - 1991;
+const ageRai = now - 2020;
+console.log(ageTarun, ageRai);
+console.log(ageTarun * 2, ageRai / 10, 2 ** 3);
+
+//2 ** 3 means 2 to the power of 3= 2*2*2
+
+const firstName = "Tarun";
+
+const lastName = "rai";
+
+console.log(firstName + " " + lastName);
+
+//Assignment operator
+
+let x = 10 + 5;
+x += 10; // x=x+10
+x *= 10;
+x++;
+x--;
+
+// Comparison Operator
+
+console.log(ageTarun > ageRai);
+
+console.log(ageRai >= 18);
+
+const isFullAge = ageRai >= 18;
+
+
+
+const now = 2037;
+const ageTarun = now - 1991;
+const ageRai = now - 2020;
+
+const averageAge = (ageTarun + ageRai) / 2;
+
+
+
+// challenge 1st
+
+const markWeight = 78;
+const markHeight = 1.69;
+
+const johnW = 92;
+const johnH = 1.95;
+
+const markBMI = markWeight / (markHeight * markHeight);
+
+const johnBMI = johnW / (johnH * johnH);
+
+console.log(markBMI, johnBMI);
+
+const markHigherBMI = markBMI > johnBMI;
+
+console.log(markHigherBMI);
+
+*/
