179af7fcc6e2b011f1e3160c3be7ad8e61beddb9
madgicaltarun <tarun.rai@madgicaltechdom.com>
2022-08-09 11:14:24 +0530

    Challenge 2 is completed of section 3

--- Javascript fundamental part -2/script.js
+++ Javascript fundamental part -2/script.js
@@ -1,8 +1,182 @@
 "use strict";
-
+/*
 let hasDriversLicense = false;
 
 const passTest = true;
 
-if (passTest) hasDriverLicense = true;
+if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log("I can drive");
+
+function logger() {
+  console.log("My name is Tarun Rai");
+}
+
+// calling / running / invoking
+
+logger();
+logger();
+logger();
+
+function foodProcessors(apples, oranges) {
+  // parameters
+  const juice = `Juice With ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+const appleJuice = foodProcessors(5, 0); //arguments
+
+console.log(appleJuice);
+
+const appleOrangeJuice = foodProcessors(2, 4);
+
+console.log(appleOrangeJuice);
+
+
+function calcAge1(birthYear) {
+  const age = 2037 - birthYear;
+  return age;
+}
+
+const age1 = calcAge1(2001);
+
+console.log("Function declaration", age1);
+
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const age2 = calcAge2(1991); // function expression return a value
+console.log("Function Expression", age2);
+
+//Arrow function
+
+const calcAge3 = (birthYear) => 2037 - birthYear;
+
+const age3 = calcAge3(1991);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirment = 65 - age;
+  return `${firstName} retires in ${retirment} years`;
+};
+
+console.log(yearsUntilRetirement(1991, "Tarun"));
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+function fruitProcessors(apples, oranges) {
+  const applepieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+  // parameters
+  const juice = `Juice With  ${applepieces} pieces of apples and ${orangePieces} of oranges`;
+  return juice;
+}
+
+console.log(fruitProcessors(2, 3));
+
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const retirment = 65 - calcAge(birthYear);
+  if (retirment > 0) {
+    return `${firstName} retires in ${retirment} years`;
+  } else {
+    return "Already retired";
+  }
+};
+console.log(yearsUntilRetirement(1991, "Tarun"));
+
+//challenge 1
+
+const calcAvg = (a, b, c) => (a + b + c) / 3;
+
+const scoreDolphins = calcAvg(44, 23, 71);
+const scoreKolas = calcAvg(65, 54, 49);
+
+const checWinner = (scoreA, scoreB) => {
+  if (scoreA >= scoreB * 2) {
+    console.log("Dolphis win the match");
+  } else if (scoreB > scoreA * 2) {
+    console.log("Kolas win the match");
+  } else {
+    console.log("No one wins the tropy");
+  }
+};
+
+checWinner(scoreDolphins, scoreKolas);
+
+const frined1 = "Tarun";
+const frined2 = "Rai";
+
+const friends = ["tarun", "abhinav", "shivam"];
+console.log(friends);
+
+const years = new Array(1991, 1984, 1999);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+
+console.log(friends[friends.length - 1]);
+
+friends[2] = "jay";
+console.log(friends);
+
+const tarun = ["tarun", "rai", 2037 - 1991, "teacher", friends];
+console.log(tarun);
+// Exercise
+
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+
+const ages = [
+  calcAge(years[0]),
+  calcAge(years[1]),
+  calcAge(years[years.length - 1]),
+];
+
+const friends = ["tarun", "abhinav", "shivam"];
+
+//Add Elements
+friends.push("Rajat");
+
+friends.unshift("John");
+
+//Remove elements
+
+friends.pop(); //last
+console.log(friends);
+
+friends.shift();
+console.log(friends);
+
+console.log(friends.indexOf("tarun"));
+
+console.log(friends.includes("tarun"));
+
+if (friends.includes("tarun")) {
+  console.log("You have a friend tarun");
+}
+
+// Challenge 2
+const calcTip = function (bill) {
+  return bill > 50 && bill < 300 ? bill * 0.15 : bill * 0.2;
+};
+
+const bills = [125, 555, 44];
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+const totals = [bills[0] + tips[0], bills[1] + tips[1].bills[2] + tips[2]];
+
+*/
