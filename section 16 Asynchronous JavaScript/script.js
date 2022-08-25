b45e3789b3ecbc61cd03a8b46a3cfdff48b8c0d5
madgicaltarun <tarun.rai@madgicaltechdom.com>
2022-08-25 12:40:26 +0530

    Section 16

---
+++ section 16 Asynchronous JavaScript/script.js
@@ -0,0 +1,86 @@
+"use strict";
+
+const btn = document.querySelector(".btn-country");
+const countriesContainer = document.querySelector(".countries");
+
+///////////////////////////////////////
+
+//
+
+const renderCountry = function (data, className = "") {
+  const html = `
+     <article class="country ${className}" >
+    <img class="country__img" src="${data.flags.svg}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name.common || data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)}</p>
+    </div>
+  </article>`;
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+/*
+const getCountryAndNeighbour = function (country) {
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    renderCountry(data);
+
+    const neighbour = data.borders?.[0];
+
+    if (!neighbour) return;
+
+    const request2 = new XMLHttpRequest();
+    request2.open("GET", `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener("load", function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+      renderCountry(data2, "neighbour");
+    });
+  });
+};
+
+getCountryAndNeighbour("portugal");
+
+*/
+
+// const request = fetch("https://restcountries.com/v2/name/portugal");
+// console.log(request);
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v2/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then((response) => response.json())
+    .then((data) => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders?.[0];
+
+      if (!neighbour) return;
+
+      fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`)
+        .then((response) => response.json())
+        .then((data) => renderCountry(data[0], "neighbour"));
+    });
+};
+
+getCountryData("usa");
